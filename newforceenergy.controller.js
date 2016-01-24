7e976fce54c59013b15858ed5dfe836a82225a3c
Jeremy Roelfs <jeremyroelfs@icloud.com>
2016-01-24 14:17:58 -0700

    Redux

---
+++ newforceenergy.controller.js
@@ -0,0 +1,40 @@
+'use strict';
+
+
+//Setup our app's controllers -------------------------------------------
+angular.module("NewForceEnergyApp").controller( 'NewForceEnergy', ['$scope', NewForceEnergy] );
+
+
+
+//Function that takescare of our controller------------------------------
+function NewForceEnergy($scope){
+
+	//vm for view model -------------
+	var vm = this;
+
+
+	
+	//define variables
+	
+	
+
+
+
+	//Setup basic database stuff
+
+		
+
+
+};//End Ex1Controller
+
+
+
+
+
+
+
+
+
+
+
+/*END OF FILE*/
